806af70d9a75eb1a2a9bd8b0d3888fabcb132511
vasu-bhimani-itp <Vasu.Bhimani@intuitive.AI>
2026-04-24 22:57:28 +0530

    first commit

---
+++ modules/workbench/main.tf
@@ -0,0 +1,4 @@
+resource "google_compute_address" "static" {
+  name = "wbi-test-default"
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "gcp_region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "gcp_project_id" {
+  type = string
+  default = ""
+}
